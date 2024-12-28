b0fcd7cd67317df54ed6df21f7de6e074e82dcb9
Andreas Prechtl <andreas.prechtl@online.de>
2024-12-28 15:04:45 +0100

    revisited Singleton pattern

--- Core/AP/ComponentModel/Singleton.cs
+++ Core/AP/ComponentModel/Singleton.cs
@@ -3,52 +3,60 @@
 namespace AP.ComponentModel;
 
 public abstract class Singleton<TSingleton> : FinalizableObject
-    where TSingleton : Singleton<TSingleton>
+    where TSingleton : notnull, Singleton<TSingleton>
 {
     public static readonly object SyncRoot = new();
 
-    private static volatile TSingleton _instance;
+    private static volatile TSingleton? s_instance;
 
     protected Singleton()
     {
-        if (_instance == null)
-        {
-            lock (SyncRoot)
-            {
-                if (_instance == null)
-                    _instance = (TSingleton)this;
-            }
-        }
-        else
+        if (!TrySetInstance(out _))
             throw new InvalidOperationException("MultiSingleton");
     }
 
-    public static void Release() => _instance.TryDispose();
+    public static void Release() => s_instance?.TryDispose();
+
+    private static bool TrySetInstance(out TSingleton instance)
+    {
+        var existingInstance = s_instance;
+        if (existingInstance is not null)
+        {
+            instance = existingInstance;
+            return false;
+        }
+        lock (SyncRoot)
+        {
+            if (s_instance is null)
+            {
+                s_instance = instance = New.Instance<TSingleton>();
+                return true;
+            }
+            else
+            {
+                instance = s_instance!;
+                return false;
+            }
+        }
+    }
 
     public static TSingleton Instance
     {
         get
         {
-            TSingleton instance = _instance;
-
-            if (instance == null)
-            {
-                lock (SyncRoot)
-                {
-                    if (_instance == null)
-                        return New.Instance<TSingleton>();
-                }
-            }
-
-            return instance;
+            TrySetInstance(out var instance);
+            return instance!;
         }
     }
+
     protected override void CleanUpResources()
     {
         base.CleanUpResources();
 
         lock (SyncRoot)
-            if (_instance == this)
-                _instance = null;
+        {
+            if (s_instance == this)
+                s_instance = null;
+        }
     }
 }
